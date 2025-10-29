aaeff2ecde1a1240d45bee941968da0f7c975d44
Robert Thompson <roberttco@gmail.com>
2025-10-29 14:36:02 -0600

    Add configuration header for WiFi and Home Assistant
    Define constants for WiFi and Home Assistant configuration.

---
+++ include/secret.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "<your SSID>"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "<your password>"
+
+#endif
+#ifndef OPEN_WEATHER_MAP_APP_ID
+#define OPEN_WEATHER_MAP_APP_ID "<your API key>"
+#endif
+
+#ifndef HOME_ASSISTANT_AUTH
+#define HOME_ASSISTANT_AUTH "Bearer <your HA api token>"
+#endif
+
+#ifndef HOME_ASSISTANT_SERVER
+#define HOME_ASSISTANT_SERVER "<your HA server name or IP>"
+#endif
+
+#ifndef HOME_ASSISTANT_PORT
+//#define HOME_ASSISTANT_PORT 443
+#define HOME_ASSISTANT_PORT 8123
+#endif
+
+#ifndef HOME_ASSISTANT_PROTOCOL
+//#define HOME_ASSISTANT_PROTOCOL "https"
+#define HOME_ASSISTANT_PROTOCOL "http"
+#endif
+
